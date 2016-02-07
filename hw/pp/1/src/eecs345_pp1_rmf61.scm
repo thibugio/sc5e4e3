22e5fef04fc4231082a1a4b49de40304c7d5b0aa
thibugio <rmf61@case.edu>
2016-02-06 23:25:04 -0500

    asdf

--- hw/pp/1/src/eecs345_pp1_rmf61.scm
+++ hw/pp/1/src/eecs345_pp1_rmf61.scm
@@ -32,15 +32,76 @@
         (cond
             ((eq? 'var (statement-type statement)) (eval-var statement state))
             ((eq? '= (statement-type statement)) (eval-assign statement state))
-            ((eq? 'if (statement-tyep statement)) (eval-cond statement state))
+            ((eq? 'if (statement-type statement)) (eval-cond statement state))
             ((eq? 'while (statement-type statement)) (eval-loop statement state))
             ((eq? 'return (statement-type statement)) (eval-return statement state))
             (else (error 'unknown "Unknown Statement Type")))))
+
+; M_bool function
+; returns whether the given condition is satisfied in the given state
+(define m-bool
+  (lambda (condition state) ))
 
 ; M_value function
 ; takes a statment and a state and returns the value of executing the statement
 (define m-value
     (lambda (statement state)
+        (cond )))
+
+; denotational semantics of variable-assignment
+; Mstate (= variable expression) = {
+; 
+; }
+(define m-state-assign
+    (lambda (variable expression state)
+        (cond )))
+
+; denotational semantics of if-statement
+; Mstate (if condition then-statement optional-else-statement) = {
+;
+; }
+(define m-state-if
+    (lambda (condition then-statement optional-else-statement)
+        (cond )))
+
+; denotational semantics of return-expression
+; Mstate (return expression) = {
+; 
+; }
+(define m-state-return
+    (lambda (expression) 
+        (cond )))
+
+; denotational semantics of while-loop
+; Mstate (while <cond> <stmt>, S) = {
+;   if Mbool (<cond>, S) = true
+;     Mstate (while <cond> <stmt>, Mstate (<stmt>, S))
+;   else
+;     S
+; }
+(define m-state-while
+  (lambda (condition statement state)
+    (if (m-bool condition state)
+        (m-state-while condition statement (m-state statement state))
+        state)))
+
+; denotational semantics of variable-declaration
+; Mstate(var <variable>, S) = {
+;   if (lookup <variable>, S) = true
+;     return 'error
+;   return (add (<variable>, 'Undefined), S) // a new state
+; }
+; 
+; Mstate(var <variable> <value>) = {
+;   if (lookup <variable>, S) = true
+;     return 'error
+;   return (add (<variable>, (Mvalue <value>, S)), S) // a new state
+; }
+(define m-state-var
+    (lambda (var)
+        (cond )))
+(define m-state-var-value
+    (lambda (var value)
         (cond )))
 
 ; evaluate a variable declaration
