327b6137cd82f2e42e7b5eb308f67d70c2c2ac24
thibugio <rmf61@case.edu>
2016-02-12 13:39:16 -0500

    cleaned up directory

--- hw/pp/1/src/mName.scm
+++
@@ -1,9 +0,0 @@
-; collection of functions used for Mname
-
-(load "mStateCore.scm")
-
-(define m-name
-    (lambda (variable state)
-        (if (state-lookup? variable state)
-            variable
-            (error 'Mname "Variable not found in the state"))))
