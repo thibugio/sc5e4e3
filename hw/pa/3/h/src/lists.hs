8cefdbb24b9e2753ba7c401fb6fdf26af07c3dc0
thibugio <rmf61@case.edu>
2016-04-06 18:19:34 -0400

    foo

--- hw/pa/3/h/src/lists.hs
+++ hw/pa/3/h/src/lists.hs
@@ -1,3 +1,14 @@
-data List a = Element a |
-              SubList [List a] deriving (Eq, Show, Read)
+data ListElement a = Element a |
+                     SubList [ListElement a] deriving (Eq, Show, Read)
 
+append [] l = l
+append l1 l2 = head l1 : append (tail l1) l2 
+
+-- Usage: flatten [Element 1,Element 3,SubList [Element 4,SubList [SubList [Element 5],SubList []]],Element 6] id
+--          => [Element 1,Element 3,Element 4,Element 5,Element 6]
+flatten [] ret = ret []
+flatten ((Element a):rest) ret = flatten rest (\v -> (ret ((Element a):v)))
+flatten ((SubList a):rest) ret = flatten rest (\v1 -> (flatten a (\v2 -> (ret (append v2 v1)))))
+
+myreverse [] acc = acc
+myreverse (a:as) acc = myreverse as (a:acc)
