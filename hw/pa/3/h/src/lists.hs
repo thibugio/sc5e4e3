a5191cbe8710319ddd4b1be4252800283a5956c0
thibugio <rmf61@case.edu>
2016-04-06 18:26:13 -0400

    finished part 2

--- hw/pa/3/h/src/lists.hs
+++ hw/pa/3/h/src/lists.hs
@@ -10,5 +10,8 @@ flatten [] ret = ret []
 flatten ((Element a):rest) ret = flatten rest (\v -> (ret ((Element a):v)))
 flatten ((SubList a):rest) ret = flatten rest (\v1 -> (flatten a (\v2 -> (ret (append v2 v1)))))
 
+-- Usage: myreverse [Element 1,Element 3,SubList [Element 4,SubList [SubList [Element 5],SubList []]],Element 6] []
+--          => [Element 6,SubList [SubList [SubList [],SubList [Element 5]],Element 4],Element 3,Element 1]
 myreverse [] acc = acc
-myreverse (a:as) acc = myreverse as (a:acc)
+myreverse ((Element a):rest) acc = myreverse rest ((Element a):acc)
+myreverse ((SubList a):rest) acc = myreverse rest (append ((SubList (myreverse a [])):[]) acc)
